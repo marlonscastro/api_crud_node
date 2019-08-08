778dd52706aaec9c23a8b9c2200458e76a03b759
marlon1611 <marlonsolucoes@hotmail.com>
2019-08-08 19:31:51 -0300

    atulizacao 1

---
+++ src/controllers/authController.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const User = require('../models/user');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    try{
+        const user = User.create(req.body);
+        return res.send({ user });
+    }
+    catch (err) {
+        return res.status(400).send({ error: 'registration failed'});
+    }
+});
